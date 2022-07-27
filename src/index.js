f9e8734cdc3dbbb82f2671abde050059ee03776c
Stefan0712 <73611325+Stefan0712@users.noreply.github.com>
2022-07-27 21:40:24 +0300

    Added basename to the BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Search from './components/Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
 
 <Routes>
   <Route path="/" element={<App />}>
